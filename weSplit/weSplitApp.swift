bfa0770acecfec29215510d884ad046dad6a3a4f
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-11-03 09:18:56 -0400

    Initial Commit

---
+++ weSplit/weSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  weSplitApp.swift
+//  weSplit
+//
+//  Created by Montserrat Gomez on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct weSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
